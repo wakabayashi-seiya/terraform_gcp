f30fe1d14833bd2faa0606d996d88b8b2267ac54
Seiya <e1517234@soka-u.jp>
2020-02-18 08:33:40 +0000

    add: gceインスタンスno

--- prod/gcp_provider.tf
+++ prod/gcp_provider.tf
@@ -1,4 +1,27 @@
 provider "google" {
   project = "${var.project_name}"
   region = "${var.region}"
+  credentials = "${file("proud-command-267623-c03d54a84755")}"
+}
+
+resource "google_compute_instance" "apps_gcp_terraform" {
+  name         = "${var.gce_name}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.zone}"
+
+  boot_disk {
+	  initialize_params {
+		size  = 30
+		type  = "${var.boot_disk_type}"
+		image = "${var.boot_disk_image}"
+	  }
+	}
+	
+  network_interface {
+    network = "default"
+  }
+
+  service_account {
+    scopes = ["logging-write", "monitoring-write"]
+  }
 }
--- prod/variables.tf
+++ prod/variables.tf
@@ -1,2 +1,7 @@
 variable "project_name" {}
 variable "region" {}
+variable "gce_name" {}
+variable "zone" {}
+variable "machine_type" {}
+variable "boot_disk_type" {}
+variable "boot_disk_image" {}
