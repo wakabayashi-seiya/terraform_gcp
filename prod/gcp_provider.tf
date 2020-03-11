f757e56779f33c2fabd8a8eed9c51ff0b897a38f
Seiya <e1517234@soka-u.jp>
2020-03-11 05:42:57 +0000

    update: 固定外部IPとファイアウォールルールの設定

--- prod/gcp_provider.tf
+++ prod/gcp_provider.tf
@@ -8,6 +8,7 @@ resource "google_compute_instance" "apps_gcp_terraform" {
   name         = "${var.gce_name}"
   machine_type = "${var.machine_type}"
   zone         = "${var.zone}"
+  tags = ["http-server", "https-server"]
 
   boot_disk {
 	  initialize_params {
@@ -18,6 +19,33 @@ resource "google_compute_instance" "apps_gcp_terraform" {
 	}
 	
   network_interface {
-    network = "default"
+    network      = "default"
+    access_config {
+    nat_ip       = "${var.nat_ip}"
+    network_tier = "${var.network_tier}"
+            }
+    }
+}
+
+
+resource "google_compute_firewall" "allow-http" {
+  name    = "default-allow-http"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
   }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["http-server"]
+}
+
+resource "google_compute_firewall" "allow-https" {
+  name    = "default-allow-https"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["https-server"]
 }
--- prod/variables.tf
+++ prod/variables.tf
@@ -6,3 +6,5 @@ variable "machine_type" {}
 variable "boot_disk_type" {}
 variable "boot_disk_image" {}
 variable "credential" {}
+variable "nat_ip" {}
+variable "network_tier" {}
